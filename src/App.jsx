16688a0a8a53f061bc027cb1daf29d773525ee79
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-08 09:00:09 +0200

    added h2 for the 'badge table' named SQUARE and PILL

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ function App() {
 
   return (
     <>
+    <h1 className="h1-badges">Badges</h1>
     <div className="all-badges">
+      <div className="badges-h2-div">
+      <h2 className="h2-badge">Square</h2>
+      <h2 className="h2-badge">Pill</h2>
+      </div>
       {colors.map((one) => (
         <div className="badges-div">
           <Badge variant="squared" color={one}>Badge</Badge>
