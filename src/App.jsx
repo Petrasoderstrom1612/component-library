fa00b9295f642c018e83cd7389106bc46a587bc4
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-08 08:18:54 +0200

    I got the badges to be working, started with two sample colors.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { useState } from 'react'
-import Badge from './Badges/Badge'
+import { useState } from "react";
+import Badge from "./Badges/Badge";
 
 function App() {
+  const colors = ["red", "yellow"];
 
   return (
     <>
-    <div className="badges-div">
-      <Badge variant="squared">Badge</Badge>
-      <Badge variant="rounded">Badge</Badge>
+    <div className="all-badges">
+      {colors.map((one) => (
+        <div className="badges-div">
+          <Badge variant="squared" color={one}>Badge</Badge>
+          <Badge variant="rounded" color={one}>Badge</Badge>
+        </div>
+      ))}
     </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Badges/Badge.jsx
+++ src/Badges/Badge.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const Badge = ({children, ...rest}) => {
+  console.log(rest)
   let variantClass = rest.variant && `${rest.variant}`
 
   return (
-    <div className={`badge ${variantClass}`}>
+    <div className={`badge ${variantClass} ${rest.color}`}>
       {children}
     </div>
   )
