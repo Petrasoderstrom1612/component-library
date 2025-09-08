9e239890457ee2caf152e5a19973ccd32d5f714b
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-08 10:57:14 +0200

    css fixes for badges and starter for banners

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
 import { useState } from "react";
 import Badge from "./Badges/Badge";
+import Banner from "./Banners.jsx/Banner";
 
 function App() {
   const colors = ["gray", "red", "yellow", "green", "blue", "indigo", "purple"];
 
   return (
     <>
-    <h1 className="h1-badges">Badges</h1>
-    <div className="all-badges">
-      <div className="badges-h2-div">
-      <h2 className="h2-badge">Square</h2>
-      <h2 className="h2-badge">Pill</h2>
-      </div>
-      {colors.map((one) => (
-        <div className="badges-div">
-          <Badge variant="squared" color={one}>Badge</Badge>
-          <Badge variant="rounded" color={one}>Badge</Badge>
+      <>
+        <h1 className="h1-badges">Badges</h1>
+        <div className="all-badges">
+          <div>
+            <h2 className="h2-badge">Square</h2>
+            <h2 className="h2-badge">Pill</h2>
+          </div>
+          {colors.map((one) => (
+            <div className="badges-div">
+              <Badge variant="squared" color={one}>
+                Badge
+              </Badge>
+              <Badge variant="rounded" color={one}>
+                Badge
+              </Badge>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
+        <hr/>
+      </>
+
+      <>
+      <h2 className="h1-banners">Banner</h2>
+          <Banner>Success</Banner>
+          <Banner>Warning</Banner>
+          <Banner>Error</Banner>
+          <Banner>Neutral</Banner>
+      </>
     </>
   );
 }
---
+++ src/Banners.jsx/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = ({children}) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default Banner
