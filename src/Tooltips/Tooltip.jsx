6c92719e666bab8f354e0a375c81bebae00995c4
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-11 16:40:20 +0200

    styling done on the text in tooltips

--- src/Tooltips/Tooltip.jsx
+++ src/Tooltips/Tooltip.jsx
@@ -1,4 +1,6 @@
 import { useRef, useState, useEffect } from "react";
+import folderIcon from "./icons/folder.svg"
+import xIcon from "./icons/X.svg"
 
 const Tooltip = ({ palette, title, paragraph }) => {
   const divRef = useRef(null);
@@ -14,8 +16,14 @@ const Tooltip = ({ palette, title, paragraph }) => {
   return (
     <div className={`palette-div ${palette}`} ref={divRef}>
       <div className="tooltip" style={{ borderTop: `18px solid ${bgColor}` }}></div>
-      <h3>{title}</h3>
-      <p>{paragraph}</p>
+      <div className="tooltip-text-div">
+        <div className="icons-titel-tooltip-div">
+            <img className={{palette}} src={folderIcon} alt="Folder" />
+            <h3 className="tooltip-h3">{title}</h3>
+            <img className={`cross-icon-tooltip ${palette}`} src={xIcon} alt="Folder" />
+        </div>
+        <p className="tooltip-p">{paragraph}</p>
+      </div>
     </div>
   );
 };
