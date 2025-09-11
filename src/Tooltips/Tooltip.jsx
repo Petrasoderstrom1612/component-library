eb3608521f7d6539873f10d5275cea40e62b37ae
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-11 13:28:48 +0200

    tooltips are there with useEffect as it needs to wait until the background colors are loaded

--- src/Tooltips/Tooltip.jsx
+++ src/Tooltips/Tooltip.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import { useRef, useState, useEffect } from "react";
 
-const Tooltip = ({ palette, title, paragraph}) => {
+const Tooltip = ({ palette, title, paragraph }) => {
+  const divRef = useRef(null);
+  const [bgColor, setBgColor] = useState("black");
+
+  useEffect(() => {
+    if (divRef.current) {
+      const color = getComputedStyle(divRef.current).backgroundColor;
+      setBgColor(color);
+    }
+  }, [palette]);
+
   return (
-    <div className={`palette-div ${palette}`}>
-      <div 
-        className="tooltip" 
-        style={{ "--bg-color": getComputedStyle(document.querySelector(`.${palette}`)).backgroundColor }}>
-      </div>
+    <div className={`palette-div ${palette}`} ref={divRef}>
+      <div className="tooltip" style={{ borderTop: `18px solid ${bgColor}` }}></div>
       <h3>{title}</h3>
       <p>{paragraph}</p>
     </div>
   );
 };
 
-export default Tooltip;
+export default Tooltip
