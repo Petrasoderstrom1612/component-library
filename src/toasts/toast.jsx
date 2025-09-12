95dc5bf0689c4d2b43a15a2eecd88470bcda991e
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-12 17:01:45 +0200

    css and html mostly unique for toast

--- src/toasts/toast.jsx
+++ src/toasts/toast.jsx
@@ -4,19 +4,19 @@ import { Checked, Exclamation, Error, Info } from "../assets/icons";
 const Toast = ({...rest}) => {
 console.log(rest)
 
-let bannerClass = rest.status
+let toastClass = rest.status
 
 let statusIcon = ""
 if (rest.status === "success") statusIcon = <Checked className="icon-success"/> 
 if (rest.status === "warning") statusIcon = <Exclamation className="icon-warning"/>
+if (rest.status === "information") statusIcon = <Info className="icon-information"/>
 if (rest.status === "error") statusIcon = <Error className="icon-error"/>
-if (rest.status === "neutral") statusIcon = <Info className="icon-neutral"/>
 
   return (
-      <div className={`banner-div ${bannerClass}`}>
-        <div className="status-icon-div">{statusIcon}</div> 
-        <div className="banner-ps-div">
-            <p className="banner-p-title">{rest.title}</p>
+      <div className={`toast-div ${toastClass}`}>
+        <div className="toast-icon-div">{statusIcon}</div> 
+        <div className="toast-ps-div">
+            <p className="toast-p-title">{rest.title}</p>
             {rest.text && <p>{rest.text}</p>}
         </div>
       </div>
