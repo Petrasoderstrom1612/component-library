a16c2760b9276384de7477337eaccf1ac7cd5d97
Petra Söderström <101672601+Petrasoderstrom1612@users.noreply.github.com>
2025-09-12 09:05:33 +0200

    added some comments

--- src/Tooltips/Tooltip.jsx
+++ src/Tooltips/Tooltip.jsx
@@ -3,19 +3,19 @@ import folderIcon from "./icons/folder.svg"
 import xIcon from "./icons/X.svg"
 
 const Tooltip = ({ palette, title, paragraph, ...rest }) => {
-  const divRef = useRef(null);
-  const [bgColor, setBgColor] = useState("black");
+  const divRef = useRef(null); //it is standart to declare ref as null
+  const [bgColor, setBgColor] = useState("");
 
   useEffect(() => {
     if (divRef.current) {
-      const color = getComputedStyle(divRef.current).backgroundColor;
-      setBgColor(color);
+      const color = getComputedStyle(divRef.current).backgroundColor; //access the DOM to get backgroundcolor of the element where ref is placed
+      setBgColor(color); //save the color you get into state
     }
   }, [palette]);
 
   return (
-    <div className={`palette-div ${palette}`} ref={divRef}>
-      <div className="tooltip" style={{ borderTop: `18px solid ${bgColor}` }}></div>
+    <div className={`palette-div ${palette}`} ref={divRef}> {/*get background-color value of this element (and that one sits in palette, it is of course only one background color on an element*/}
+      <div className="tooltip" style={{ borderTop: `18px solid ${bgColor}` }}></div> {/* place the color from state to the tooltip element*/}
       <div className="tooltip-text-div">
         <div className="icons-titel-tooltip-div">
             <img className={{palette}} src={folderIcon} alt="Folder" />
